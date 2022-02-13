4bd23a94a55526587b063b014af7035a1e1ad9f2
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-02-13 12:13:01 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const app = require('express')();
+
+const data = {
+  items: [
+    {
+      id: 1,
+      name: 'Item 1',
+      price: 10,
+      quantity: 1,
+      description: 'Item 1 description',
+    },
+    {
+      id: 2,
+      name: 'Item 2',
+      price: 20,
+      quantity: 2,
+      description: 'Item 2 description',
+    },
+    {
+      id: 3,
+      name: 'Item 3',
+      price: 30,
+      quantity: 3,
+      description: 'Item 3 description',
+    },
+    {
+      id: 4,
+      name: 'Item 4',
+      price: 40,
+      quantity: 4,
+      description: 'Item 4 description',
+    },
+    {
+      id: 5,
+      name: 'Item 5',
+      price: 50,
+      quantity: 5,
+      description: 'Item 5 description',
+    },
+  ],
+};
+
+app.get('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.send(data);
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port);
